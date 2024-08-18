5ef0a5c860f9a38e449e292c1e0a5aeee028a521
Shivam Kumar <dhiryan441@gmail.com>
2024-08-18 10:36:07 +0530

    Second Commit

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const db = require('./dbconnection');
-require('dotenv').config();
-
+const db = require("./dbconnection");
+require("dotenv").config();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/",(req,res)=>{
-    res.send("connection is completed");
-})
+app.get("/", (req, res) => {
+  res.send("connection is completed");
+});
 
-  const personRoutes = require('./routes/personRoutes')
-  const menuRoutes = require('./routes/menuRoutes');
-  app.use('/person',personRoutes);
-  app.use('/menu',menuRoutes);
+const personRoutes = require("./routes/personRoutes");
+const menuRoutes = require("./routes/menuRoutes");
+app.use("/person", personRoutes);
+app.use("/menu", menuRoutes);
 
-  const PORT = process.env.PORT || 3000;
-app.listen(PORT,()=>{
-    console.log(`server is listening ${PORT}`);
-})
+// This is the Port
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`server is listening ${PORT}`);
+});
